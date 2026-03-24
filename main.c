5fadd120692c238a789399c4a7a6d8086e69bf96
NOUMEDOR-LATEY Godwin Mawugnon <noumedorlateygodwinmawugnon63@gmail.com>
2026-03-24 11:25:54 +0000

    Add files via upload

--- main.c
+++ main.c
@@ -15,6 +15,22 @@ int verification_0(int tab[], int nbVal, int x)
     }
     return 0;
 }
+
+int verificationWhile_0(int tab[], int nbVal, int x)
+{
+    int i = 1;
+    while (i <= nbVal)
+    {
+        if(tab[i] == x)
+        {
+            return 1;
+        }
+        i++;
+    }
+    return 0;
+}
+
+
 int verification_1(int tab[], int nbVal, int x)
 {
     int i;
@@ -33,19 +49,43 @@ int verification_1(int tab[], int nbVal, int x)
     }
     return trouve;
 }
+
+int verificationWhile_1(int tab[], int nbVal, int x)
+{
+    int i = 1;
+    int trouve;
+
+    while (i <= nbVal)
+    {
+        if(tab[i] == x)
+        {
+            trouve = 1;
+        }
+        else
+        {
+            trouve = 0;
+        }
+        i++;
+    }
+    return trouve;
+}
+
 int main()
 {
     int tab[] = {0,8,9,7,4};
 
     const int  nbVal = sizeof(tab)/sizeof(tab[0]);
-    int resultat;
+    int resultat,resultat_1,resultat_2,resultat_3;
 
     resultat = verification_0(tab, nbVal, 8);
-    resultat_1 = verification_1(tab, nbVal, 8);
-
+    resultat_1 = verificationWhile_0(tab, nbVal, 8);
+    resultat_2 = verification_1(tab, nbVal, 8);
+    resultat_3 = verificationWhile_1(tab, nbVal, 8);
 
     printf("\nresultat = %d", resultat);
     printf("\nresultat = %d", resultat_1);
+    printf("\nresultat = %d", resultat_2);
+    printf("\nresultat = %d", resultat_3);
 
     return 0;
 }
